8c1e18b2c3f6fb58c8414af4e918053deed763e8
lcmtwn <lcmtwn@gmail.com>
2017-04-03 13:58:22 +0800

    add list installation feature

--- webdriver_controller/controller.py
+++ webdriver_controller/controller.py
@@ -15,13 +15,17 @@ class WebdriverController(object):
                                                 config.INSTALLATION_FILE))
     __outdated_file = Path('{}.old'.format(__installation_file))
 
-    def __check_installation_file(self):
+    def __check_installation_file(self) -> None:
         if self.__installation_file.exists():
             self.__installation_file.rename(self.__outdated_file)
 
-    def __remove_outdated_installation_file(self):
+    def __remove_outdated_installation_file(self) -> None:
         if self.__outdated_file.exists():
             self.__outdated_file.unlink()
+
+    def __write_installation_file(self, version_info: dict) -> None:
+        with open(self.__installation_file, 'w') as fh:
+            json.dump(version_info, fh)
 
     def download(self):
         if not self.__installation_folder.exists():
@@ -35,19 +39,31 @@ class WebdriverController(object):
         selenium_standalone = SeleniumStandalone()
         selenium_standalone.download_driver()
 
-        versions = {
+        version_info = {
             'chrome': chromedriver.version,
             'selenium': selenium_standalone.version
         }
 
-        with open(self.__installation_file, 'w') as fh:
-            json.dump(versions, fh)
-
+        self.__write_installation_file(version_info)
         self.__remove_outdated_installation_file()
 
     def cleanup(self):
         if self.__installation_folder.exists():
             print('deleting {} ...'.format(self.__installation_folder))
             shutil.rmtree(self.__installation_folder)
+        else:
+            print('no Selenium Webdriver installation found')
+
+    def list(self):
+        if self.__installation_folder.exists():
+            if self.__installation_file.exists():
+                version_info = {}
+                with open(self.__installation_file, 'r') as fh:
+                    version_info = json.load(fh)
+
+                print('ChromeDriver version: {}'.format(version_info.get('chrome')))
+                print('Selenium standalone version: {}'.format(version_info.get('selenium')))
+            else:
+                print('no installation file found')
         else:
             print('no Selenium Webdriver installation found')
