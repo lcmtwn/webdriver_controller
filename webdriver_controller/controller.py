dd3280c9e5b386718148d5e2cc83b3db85c87eee
lcmtwn <lcmtwn@gmail.com>
2017-04-03 11:29:00 +0800

    rewrite drivers

--- webdriver_controller/controller.py
+++ webdriver_controller/controller.py
@@ -1,27 +1,27 @@
-import os
+from pathlib import Path
 import shutil
 
 from webdriver_controller import config
-from webdriver_controller.downloader import chromedriver
-from webdriver_controller.downloader import selenium_standalone
+from webdriver_controller.drivers.chromedriver import ChromeDriver
+from webdriver_controller.drivers.selenium_standalone import SeleniumStandalone
 
 
 class WebdriverController(object):
-    _installation_folder = '{}{}'.format(os.getcwd(),
-                                         config.INSTALLATION_FOLDER)
-
-    def __init__(self):
-        pass
+    _installation_folder = Path('{}{}'.format(Path.cwd(),
+                                              config.INSTALLATION_FOLDER))
 
     def download(self):
-        if not os.path.exists(self._installation_folder):
-            os.makedirs(self._installation_folder)
+        if not self._installation_folder.exists():
+            self._installation_folder.mkdir()
 
-        chromedriver.download()
-        selenium_standalone.download()
+        chromedriver = ChromeDriver()
+        chromedriver.download_driver()
+
+        selenium_standalone = SeleniumStandalone()
+        selenium_standalone.download_driver()
 
     def cleanup(self):
-        if os.path.exists(self._installation_folder):
+        if self._installation_folder.exists():
             print('deleting {} ...'.format(self._installation_folder))
             shutil.rmtree(self._installation_folder)
         else:
